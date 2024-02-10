c5e0935083d47a89e00d4e4ddefe5cdf839c61a9
omerzan <omer.c.koca@gmail.com>
2024-02-11 00:10:43 +0300

    Add project files.

---
+++ Workaround/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using Business.Concrete;
+using Entities.Concrete;
+
+
+Person person1 = new Person();
+person1.FirstName = "ÖMER";
+person1.LastName = "KOCA";
+person1.NationalIdentity = 123;
+person1.DateofBirth = 1998;
+
+PttManager pttManager= new PttManager(new PersonManager());
+
+pttManager.GiveMask(person1);
